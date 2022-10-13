14516beea19a74b4ff645fa7b2137d157b0437e2
igorlitvak <igr.litvak@gmail.com>
2022-10-13 17:15:17 +0300

    Fix bug

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -8,4 +8,4 @@ python3 manage.py collectstatic --noinput
 
 # Run server
 # Exec is needed for gunicorn process to properly receive SIGTERM and SIGKILL
-exec python3 -m gunicorn infochemistry.wsgi --bind "0.0.0.0:8000"
+exec python3 -m gunicorn backend.wsgi --bind "0.0.0.0:8000"
