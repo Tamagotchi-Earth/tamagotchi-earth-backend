9d02b916bb743934b2583edf310b2ea89154154a
igorlitvak <igr.litvak@gmail.com>
2022-10-17 18:30:15 +0300

    Add search for products

--- backend/apps/products/views.py
+++ backend/apps/products/views.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import Product
 from .serializers import ProductSerializer
@@ -6,6 +7,8 @@ from .serializers import ProductSerializer
 class ProductViewSet(ReadOnlyModelViewSet):
     """Food and drinks (list, retrieve)"""
     serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['name']
 
     def get_queryset(self):
         return Product.objects.all()
